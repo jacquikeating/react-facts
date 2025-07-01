a1c2ee962c973079dabfbd3486e07ac0d58086a3
jacquikeating <j.keating@live.ca>
2025-07-01 00:41:31 -0400

    Created components
    - Created a /components folder.
    - Created App.jsx, Navbar.jsx, and Main.jsx.
    - Exported and imported components.
    - Moved all content into separate components.

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./react-facts/components/Navbar";
+import Main from "./react-facts/components/Main";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
+}
